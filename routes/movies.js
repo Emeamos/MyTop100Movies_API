104631db889b1aab0daf0a639aa4b4cd8418a111
Emeamos <Emediongamos.engr@gmail.com>
2023-05-13 03:21:04 +0100

    adding router

---
+++ routes/movies.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createMovie, deleteMovie, getMovieById, listMovies, syncMovies, updateMovie } from "../controller/movies.js";
+
+const movieRouter = express.Router();
+
+movieRouter.get("/", listMovies);
+movieRouter.post("/", createMovie);
+movieRouter.get("/:id", getMovieById);
+movieRouter.put("/:id", updateMovie);
+movieRouter.delete("/:id", deleteMovie);
+movieRouter.post("/sync", syncMovies)
