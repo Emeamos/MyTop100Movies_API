5032cba2fcb33ee61e4f42287f1b8a477ea03b17
Emeamos <Emediongamos.engr@gmail.com>
2023-05-14 01:04:22 +0100

    initializing swagger UI on app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import dbConnect from "./config/dbConnect.js";
 import bodyparser from "body-parser";
 import cors from "cors";
 import movieRouter from "./routes/movies.js";
+import swaggerUiExpress from "swagger-ui-express";
+import options from "./swagger.js";
 dotenv.config({path:'./.env'});
 
 dbConnect();
@@ -11,6 +13,9 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended : true}))
+
+// Initialize Swagger UI
+app.use('/api-docs', swaggerUiExpress.serve, swaggerUiExpress.setup(options));
 
 app.use("/api/v1/movies", movieRouter)
 
